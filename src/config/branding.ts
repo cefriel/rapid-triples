3b376ff76fb34f0ee88d5e61a741a4df22795151
Mario Scrocca <mario.scrocca@gmail.com>
2026-04-22 17:41:50 +0200

    Update branding.ts

--- src/config/branding.ts
+++ src/config/branding.ts
@@ -1,7 +1,7 @@
 export const brand = {
   primary: '#002060',
   secondary: '#FFDC06',
-  logoUrl: '/logo.png',
+  logoUrl: `${import.meta.env.BASE_URL}logo.png`,
   appName: 'rapid-triples',
   companyName: 'Cefriel',
   companyUrl: 'https://www.cefriel.com/',
